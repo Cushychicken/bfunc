2efb39c0f0d2b9c065dfa16e9cea89fb6f656705
Nash Reilly <nrc.reilly@gmail.com>
2020-03-16 08:09:50 -0400

    Added packet flagging to CDC_Receive_FS() callback funciton

--- sw_embedded/bfunc_rev1/Src/usbd_cdc_if.c
+++ sw_embedded/bfunc_rev1/Src/usbd_cdc_if.c
@@ -122,6 +122,7 @@
 /* It's up to user to redefine and/or remove those define */
 /** Received data over USB are stored in this buffer      */
 uint8_t UserRxBufferFS[APP_RX_DATA_SIZE];
+volatile uint8_t usb_packet_flag = 0;
 
 /** Data to send over USB CDC are stored in this buffer   */
 uint8_t UserTxBufferFS[APP_TX_DATA_SIZE];
@@ -293,6 +294,7 @@ static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
   CDC_Transmit_FS(Buf, *Len);
   USBD_CDC_SetRxBuffer(&hUsbDeviceFS, &Buf[0]);
   USBD_CDC_ReceivePacket(&hUsbDeviceFS);
+  usb_packet_flag = 1; 
   return (USBD_OK);
   /* USER CODE END 6 */
 }
